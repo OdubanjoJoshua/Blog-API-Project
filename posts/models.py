e8b903b2582cf8cd7ee88ef2eb5b9220c5e3eae0
OdubanjoJoshua <joshuaodubanjo@yahoo.com>
2023-02-04 16:22:59 +0100

    made few changes in post app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    uploaded_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
