a695a02ea80aa2e34fc0cfdeeeb91c94ea6f1e13
OdubanjoJoshua <joshuaodubanjo@gmail.com>
2023-02-06 10:12:51 +0100

    Post app basic setup

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . views import *
+
+urlpatterns = [
+    path('', PostList.as_view(), name='post-list'),
+    path('<int:pk>/',PostDetail.as_view(), name='post-detail')
+]
